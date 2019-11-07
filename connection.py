cbbdee2d5e9bd6965c35639dbbc7a8bafeaf9b4b
Aidil Rezjki S S <riskisultan@yahoo.com>
2019-11-07 16:52:12 +0700

    Unmask done.

--- connection.py
+++ connection.py
@@ -1,3 +1,5 @@
+import base64
+import hashlib
 import threading
 
 class WebConnection(threading.Thread):
@@ -10,29 +12,31 @@ class WebConnection(threading.Thread):
     def run(self):        
         data = self._connection.recv(2**32 - 1)
         handshakeRequest = self.parseHandshake(data)
-        print(handshakeRequest)
         if handshakeRequest is None:
-            self._connection.send(self.getDenyHandshakeResponse())
+            self._connection.send(bytes(self.getDenyHandshakeResponse(), 'utf-8'))
             return
         
-        self._connection.send(self.getHandshakeResponse(handshakeRequest))
+        self._connection.send(bytes(self.getHandshakeResponse(handshakeRequest), 'utf-8'))
 
         while True:
-            data = _connection.recv(2**32 - 1)
+            data = self.parseFrame(self._connection.recv(2**32 - 1))
             if not data:
                 break
+            data['payload'] = self.unmaskPayload(data['mask'], data['payload'])
+            print(data)
 
     def parseHandshake(self, data):
-        data = str(data)
+        data = data.decode('utf-8')
         httpHandshake = data.split('\r\n')
-        if 'Connection: Upgrade' in httpHandshake and 'Upgrade: Websocket' in httpHandshake:
+        if 'Connection: Upgrade' in httpHandshake and 'Upgrade: websocket' in httpHandshake:
             httpHandshake = httpHandshake[1:]
             headers = {}
             for headerString in httpHandshake:
-                header, value = headerString.split(' ')
-                header[:len(header) - 1]
+                if headerString == '' or headerString == ' ':
+                    continue
+                header, value = headerString.split(' ', 1)
+                header = header[:len(header) - 1]
                 headers[header] = value
-            
             return headers
         return None
     
@@ -40,7 +44,7 @@ class WebConnection(threading.Thread):
         httpResponse =  'HTTP/1.1 101 Switching Protocol\r\n' + \
                         'Upgrade: websocket\r\n' + \
                         'Connection: Upgrade\r\n' + \
-                        'Sec-WebSocket-Accept: %s\r\n\r\n' %(self.generateResponseKey(headers['Sec-WebSocket-Key']))
+                        'Sec-WebSocket-Accept: %s\r\n\r\n' %((self.generateResponseKey(headers['Sec-WebSocket-Key'])).decode())
         
         return httpResponse
     
@@ -51,12 +55,26 @@ class WebConnection(threading.Thread):
                         'Incorrect request'
         
         return httpResponse
-                            
     
     def generateResponseKey(self, key):
         WS_MAGIC_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
         key += WS_MAGIC_KEY
-        return base64.standard_b64encode(hashlib.sha1(key).digest())
+        key = key.encode()
+        return base64.b64encode(hashlib.sha1(key).digest())
+    
+    def unmaskPayload(self, mask, payload):
+        decodedPayload = bytearray()
+        print('Mask ', mask)
+
+        i = 0
+        for byte in payload:
+            maskkk = (mask >> ((3 - i) * 8)) & 0xff
+            decodedPayload.append(byte ^ maskkk)
+            i += 1
+            if (i >= 4):
+                i = 0
+            
+        return decodedPayload
     
     def parseFrame(self, data):
         bytePointer = 0
@@ -85,13 +103,10 @@ class WebConnection(threading.Thread):
         mask = 0
         if useMask:
             for i in range(0, 4):
-                if i == 3:
-                    mask = int(data[bytePointer])
-                else:
-                    mask = mask << 8 + int(data[bytePointer])
+                mask = (mask << 8) + int(data[bytePointer])
                 bytePointer += 1
 
-        payload = data[bytePointer:len(data) - 1]
+        payload = data[bytePointer:len(data)]
 
         return {
             'fin': fin,
@@ -101,5 +116,24 @@ class WebConnection(threading.Thread):
             'mask': mask,
             'payload': payload,
         }
-    
+
+    def buildFrame(self, frameDict):
+        payloadData = bytearray(0)
+        payloadData.append(frameDict['fin'])
+        payloadData.append(frameDict['opCode'])
+        payloadData.append(frameDict['useMask'])
+        
+        if frameDict['length'] < 126:
+            payloadData.append(frameDict['length'])
+        elif frameDict['length'] < 65536:
+            payloadData.append(bytes(126))
+            payloadData.append(frameDict['length'].to_bytes(2, 'little'))
+        else:
+            payloadData.append(bytes(127))
+            payloadData.append(frameDict['length'].to_bytes(8, 'little'))
+
+        payloadData.append(frameDict['payload'])
+        return payloadData
+        
+
 
